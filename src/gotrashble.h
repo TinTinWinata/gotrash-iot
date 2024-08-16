e366936de4c6bf0d450b4019a80c01b9a05a9fab
TinTinWinata <justine.purwoko@binus.ac.id>
2024-08-16 19:54:30 +0700

    feat: success receive data logic

--- src/gotrashble.h
+++ src/gotrashble.h
@@ -1,48 +1,18 @@
+#ifndef GOTRASHBLE_H
+#define GOTRASHBLE_H
+
+#include <map> 
 #include <ArduinoBLE.h>
 #include <Arduino.h>
 
-class GoTrashBLE {
-  private:
-    String participants[10];
-    BLEService service;
-    BLECharacteristic characteristic;
-
-  public:
-    GoTrashBLE()
-      : service("180D"), 
-        characteristic("2A37", BLERead | BLEWrite | BLEWriteWithoutResponse, 20) {}
-
-    void setup() {
-      BLE.begin();
-      BLE.setLocalName("GoTrash");
-      BLE.setDeviceName("GoTrash");
-      BLE.setAdvertisedService(service);
-
-      characteristic.canWrite();
-      characteristic.canRead();
-
-      service.addCharacteristic(characteristic);
-      BLE.addService(service);
-      BLE.advertise();
-      Serial.println("GoTrash BLE Started!");
-    }
-
-    void loop() {
-      BLEDevice central = BLE.central();
-      Serial.println("Checking Central ...");
-      if (central && central.connected() && characteristic.written()) {
-        const uint8_t* value = characteristic.value();
-        int length = characteristic.valueLength();
-
-        // Convert uint8_t array to String
-        String receivedValue = "";
-        for (int i = 0; i < length; i++) {
-          receivedValue += (char)value[i];
-        }
-        Serial.println("Received value: " + receivedValue);
-      }
-      delay(1000); 
-    }
+struct User {
+  int id;
+  unsigned long timestamp;
 };
 
-extern GoTrashBLE ble;
+extern std::map<int, unsigned long> userMap;
+
+void setupBLE();
+void loopBLE();
+
+#endif // GOTRASHBLE_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,7 @@
 #include <Arduino.h>
 #include <ArduinoBLE.h>
+#include <gotrashble.h>
 
-// Prototype Function
-void setupBLE();
-void loopBLE();
-
-BLEService myService("180D");
-BLECharacteristic myCharacteristic("2A37", BLERead | BLEWrite | BLEWriteWithoutResponse, 20);
-
-void setupBLE(){
-  BLE.begin();
-  BLE.setLocalName("GoTrash");
-  BLE.setDeviceName("GoTrash");
-  BLE.setAdvertisedService(myService);
-  
-  myCharacteristic.canWrite();
-  myCharacteristic.canRead();
-  myCharacteristic.writeValue("Hello World");
-
-  myService.addCharacteristic(myCharacteristic);
-  BLE.addService(myService);
-  BLE.advertise();
-  Serial.println("BLE Peripheral started advertising");
-}
-
-void loopBLE(){
-  BLEDevice central = BLE.central();
-  Serial.println("Looping BLE ...");
-  if (central) {
-    Serial.println("Connected to central. ");
-    Serial.println(central.address());
-
-    while (central.connected()) {
-      // Check for incoming data or perform actions
-      if (myCharacteristic.written()) {
-        const uint8_t* value = myCharacteristic.value();
-        int length = myCharacteristic.valueLength();
-
-        // Convert uint8_t array to String
-        String receivedValue = "";
-        for (int i = 0; i < length; i++) {
-          receivedValue += (char)value[i];
-        }
-        Serial.println("Received value: " + receivedValue);
-      }
-    }
-    Serial.println("Disconnected from central.");
-  }
-  delay(1000); 
-}
 
 void setup() {
   Serial.begin(921600);
